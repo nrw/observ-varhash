71e395473fe2771b1a0e898cd313bd78cc5ca987
Nicholas Westlake <nicholasredlin@gmail.com>
2014-06-05 21:20:31 -0500

    refactor

--- index.js
+++ index.js
@@ -9,14 +9,12 @@ function ObservVarhash(hash, createFn) {
   createFn = createFn || function (obj) { return obj }
 
   var keys = Object.keys(hash)
-
   var initialState = {}
 
   keys.forEach(function (key) {
     if (key === 'name') {
-      throw new Error('cannot create an observ-varhash ' +
-        'with a key named "name". Clashes with ' +
-        '`Function.prototype.name`.');
+      throw new Error('cannot create an observ-varhash with a key named' +
+        '"name". Clashes with `Function.prototype.name`.')
     }
 
     var observ = hash[key]
@@ -28,7 +26,6 @@ function ObservVarhash(hash, createFn) {
   obs.get = get
   obs.delete = del
   obs.put = put(createFn)
-
   obs._removeListeners = {}
 
   keys.forEach(function (key) {
@@ -36,36 +33,30 @@ function ObservVarhash(hash, createFn) {
     obs[key] = createFn(observ, key)
 
     if (typeof observ === 'function') {
-      obs._removeListeners[key] = observ(_watch(obs, key))
+      obs._removeListeners[key] = observ(watch(obs, key))
     }
   })
 
   return obs
 }
 
+// api
+function get (key) { return this[key] }
+
 function put (createFn) {
   return function (key, val) {
     var obs = this
-    var state = extend(obs())
     var observ = createFn(val, key)
-
-    if (obs._removeListeners[key]) {
-      obs._removeListeners[key]()
-      delete obs._removeListeners[key]
-    }
-
+    var state = prepareChange(obs, key)
     var value = typeof observ === 'function' ? observ() : observ
 
     state[key] = value
 
     if (typeof observ === 'function') {
-      obs._removeListeners[key] = observ(_watch(obs, key))
+      obs._removeListeners[key] = observ(watch(obs, key))
     }
 
-    var diff = {}
-    diff[key] = value && value._diff ? value._diff : value
-
-    state._diff = diff
+    state._diff = diff(key, value)
     obs.set(state)
     obs[key] = observ
 
@@ -73,41 +64,41 @@ function put (createFn) {
   }
 }
 
-function get (key) {
-  return this[key]
+function del (key) {
+  var state = prepareChange(this, key)
+
+  delete state[key]
+  state._diff = diff(key, TOMBSTONE)
+  this.set(state)
+
+  return this
 }
 
-function del (key) {
-  var obs = this
-  var modified = obs()
+// helpers
+function watch (obs, key, state) {
+  return function (value) {
+    var state = extend(obs())
+    state[key] = value
+    state._diff = diff(key, value)
+    obs.set(state)
+  }
+}
+
+function prepareChange (obs, key) {
+  var state = extend(obs())
 
   if (obs._removeListeners[key]) {
     obs._removeListeners[key]()
     delete obs._removeListeners[key]
   }
 
-  delete modified[key]
-
-  var diff = {}
-  diff[key] = TOMBSTONE
-
-  modified._diff = diff
-
-  obs.set(modified)
-  return obs
+  return state
 }
 
-function _watch (obs, key) {
-  return function (value) { return _rewatch(obs, key, value) }
-}
-
-function _rewatch (obs, key, value) {
-  var state = extend(obs())
-  state[key] = value
-  var diff = {}
-  diff[key] = value && value._diff ? value._diff : value
-  state._diff = diff
-  obs.set(state)
+function diff (key, value) {
+  var obj = {}
+  obj[key] = value && value._diff ? value._diff : value
+  return obj
 }
 
 function Tombstone () {
