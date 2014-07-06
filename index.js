b91e6c25a2ad75998263da28d02aa0e5ca649ac2
Nicholas Westlake <nicholasredlin@gmail.com>
2014-07-06 02:02:13 -0500

    translate

---
+++ index.js
@@ -0,0 +1,142 @@
+var Observ = require('observ')
+var extend = require('xtend')
+
+ObservVarhash.Tombstone = new Tombstone()
+
+module.exports = ObservVarhash
+
+function ObservVarhash (hash, createValue) {
+  createValue = createValue || function (obj) { return obj }
+
+  var initialState = {}
+
+  for (var key in hash) {
+    var observ = hash[key]
+    checkKey(key)
+    initialState[key] = isFunc(observ) ? observ() : observ
+  }
+
+  var obs = Observ(initialState)
+  obs._removeListeners = {}
+
+  var actions = methods(createValue)
+  for (var k in actions) {
+    obs[k] = actions[k]
+  }
+
+  for (key in hash) {
+    obs[key] = createValue(hash[key], key)
+
+    if (isFunc(obs[key])) {
+      obs._removeListeners[key] = obs[key](watch(obs, key))
+    }
+  }
+  return obs
+}
+
+function methods (createValue) {
+  return {
+    get: function (key) {
+      return this[key]
+    },
+
+    put: function (key, val) {
+      checkKey(key)
+
+      var observ = createValue(val, key)
+      var state = extend(this())
+
+      state[key] = isFunc(observ) ? observ() : observ
+
+      if (isFunc(this._removeListeners[key])) {
+        this._removeListeners[key]()
+      }
+
+      this._removeListeners[key] = isFunc(observ) ?
+        observ(watch(this, key)) : null
+
+      state._diff = diff(key, state[key])
+
+      this.set(state)
+      this[key] = observ
+
+      return this
+    },
+
+    'delete': function (key) {
+      var state = extend(this())
+      if (isFunc(this._removeListeners[key])) {
+        this._removeListeners[key]()
+      }
+
+      delete this._removeListeners[key]
+      delete state[key]
+
+      state._diff = diff(key, ObservVarhash.Tombstone)
+
+      this.set(state)
+
+      return this
+    }
+  }
+}
+
+function watch (obs, key) {
+  return function (value) {
+    var state = extend(obs())
+    state[key] = value
+    state._diff = diff(key, value)
+    return obs.set(state)
+  }
+}
+
+function diff (key, value) {
+  var obj = {}
+  obj[key] = value && value._diff ? value._diff : value
+  return obj
+}
+
+function checkKey (key) {
+  var msg = getKeyError(key)
+
+  if (msg) {
+    throw new Error(msg)
+  }
+}
+
+function getKeyError (key) {
+  switch (key) {
+    case 'name': {
+      return format(key, 'Clashes with `Function.prototype.name`.')
+    }
+    case 'get':
+    case 'put':
+    case 'delete':
+    case '_diff':
+    case '_removeListeners': {
+      return format(key, 'Clashes with observ-varhash method')
+    }
+    default: {
+      return ''
+    }
+  }
+}
+
+function formatError (key, reason) {
+  return 'cannot create an observ-varhash with key `' + key + '`, ' + reason
+}
+
+function isFunc (obj) {
+  return typeof obj === 'function'
+}
+
+// identify deletes
+function Tombstone () {}
+
+Tombstone.prototype.toJSON = nameTombstone
+Tombstone.prototype.inspect = nameTombstone
+Tombstone.prototype.toString = nameTombstone
+
+function nameTombstone () {
+  return '[object Tombstone]'
+}
---
+++ test/index.js
@@ -0,0 +1,349 @@
+var test = require('tape')
+var Observ = require('observ')
+var ObservStruct = require('observ-struct')
+var ObservVarhash = require('../index.js')
+
+test('ObservVarhash is a function', function (t) {
+  t.equal(typeof ObservVarhash, 'function')
+  t.end()
+})
+
+test('ObservVarhash contains correct initial value', function (t) {
+  var obj = ObservVarhash({foo: 'foo', bar: 'bar'}, function (obj, key) {
+    return Observ(obj)
+  })
+
+  var state = obj()
+
+  t.equal(state.foo, 'foo')
+  t.equal(state.bar, 'bar')
+
+  t.end()
+})
+
+test('works without create fn', function (t) {
+  var obj = ObservVarhash({foo: 'foo', bar: 'bar'})
+  var state = obj()
+
+  t.equal(state.foo, 'foo')
+  t.equal(state.bar, 'bar')
+
+  t.end()
+})
+
+test('observ emits change', function (t) {
+  var obj = ObservVarhash({
+    foo: Observ('foo'),
+    bar: Observ('bar')
+  })
+  var changes = []
+
+  obj(function (state) {
+    changes.push(state)
+  })
+
+  obj.foo.set('foo')
+  obj.foo.set('foo2')
+  obj.bar.set('bar2')
+
+  t.equal(changes.length, 3)
+  t.same(changes[0], {foo: 'foo', bar: 'bar', _diff: {foo: 'foo'}})
+  t.same(changes[1], {foo: 'foo2', bar: 'bar', _diff: {foo: 'foo2'}})
+  t.same(changes[2], {foo: 'foo2', bar: 'bar2', _diff: {bar: 'bar2'}})
+
+  t.notEqual(changes[0], changes[1])
+  t.notEqual(changes[1], changes[2])
+
+  t.end()
+})
+
+test('add key', function (t) {
+  var obj = ObservVarhash({
+    foo: Observ('foo'),
+    bar: Observ('bar')
+  })
+  var changes = []
+
+  obj(function (state) {
+    changes.push(state)
+  })
+
+  obj.put('baz', Observ('baz'))
+
+  t.equal(changes.length, 1)
+  t.same(changes[0], {foo: 'foo', bar: 'bar', baz: 'baz', _diff: {baz: 'baz'}})
+  t.same(obj(), {foo: 'foo', bar: 'bar', baz: 'baz', _diff: {baz: 'baz'}})
+
+  t.end()
+})
+
+test('emits change for added key', function (t) {
+  var obj = ObservVarhash({foo: Observ('foo')})
+  var changes = []
+
+  obj(function (state) {
+    changes.push(state)
+  })
+
+  obj.put('bar', Observ('bar'))
+  obj.get('bar').set('baz')
+
+  t.equal(changes.length, 2)
+  t.same(changes[0], {foo: 'foo', bar: 'bar', _diff: {bar: 'bar'}})
+  t.same(obj(), {foo: 'foo', bar: 'baz', _diff: {bar: 'baz'}})
+
+  t.end()
+})
+
+test('remove key', function (t) {
+  var obj = ObservVarhash({
+    foo: Observ('foo'),
+    bar: Observ('bar')
+  })
+  var changes = []
+
+  obj(function (state) {
+    changes.push(state)
+  })
+
+  t.equal(Object.keys(obj._removeListeners).length, 2)
+
+  obj.delete('foo')
+
+  t.equal(Object.keys(obj._removeListeners).length, 1)
+  t.equal(changes.length, 1)
+
+  t.same(changes[0], {bar: 'bar', _diff: {foo: ObservVarhash.Tombstone}})
+  t.same(obj(), {bar: 'bar', _diff: {foo: ObservVarhash.Tombstone}})
+
+  t.end()
+})
+
+test('supports both observs and values', function (t) {
+  var obj = ObservVarhash({
+    foo: Observ('foo'),
+    bar: 'bar'
+  })
+
+  t.equal(typeof obj.foo, 'function')
+  t.equal(obj.foo(), 'foo')
+  t.equal(obj.bar, 'bar')
+
+  t.end()
+})
+
+test('works with nested things', function (t) {
+  var obj = ObservVarhash({
+    fruits: ObservVarhash({
+      apples: Observ(3),
+      oranges: Observ(5)
+    }),
+    customers: Observ(5)
+  })
+
+  var initialState = obj()
+  var changes = []
+  var fruitChanges = []
+
+  obj(function (state) {
+    changes.push(state)
+  })
+
+  obj.fruits(function (state) {
+    fruitChanges.push(state)
+  })
+
+  obj.fruits.oranges.set(6)
+  obj.customers.set(10)
+  obj.fruits.apples.set(4)
+
+  t.equal(changes.length, 3)
+  t.equal(fruitChanges.length, 2)
+  t.notEqual(changes[0], initialState)
+  t.notEqual(changes[1], changes[0])
+  t.notEqual(changes[2], changes[1])
+  t.notEqual(fruitChanges[0], initialState.fruits)
+  t.notEqual(fruitChanges[1], fruitChanges[0])
+
+  t.same(initialState, {
+    customers: 5,
+    fruits: {
+      apples: 3,
+      oranges: 5
+    }
+  })
+  t.same(changes[0], {
+    customers: 5,
+    fruits: {
+      apples: 3,
+      oranges: 6,
+      _diff: {oranges: 6}
+    },
+    _diff: {fruits: {oranges: 6}}
+  })
+  t.same(changes[1], {
+    customers: 10,
+    _diff: {customers: 10},
+    fruits: {
+      apples: 3,
+      oranges: 6,
+      _diff: {oranges: 6}
+    }
+  })
+  t.same(changes[2], {
+    customers: 10,
+    _diff: {fruits: {apples: 4}},
+    fruits: {
+      apples: 4,
+      oranges: 6,
+      _diff: {apples: 4}
+    }
+  })
+  t.same(initialState.fruits, {
+    apples: 3,
+    oranges: 5
+  })
+  t.same(fruitChanges[0], {
+    apples: 3,
+    oranges: 6,
+    _diff: {oranges: 6}
+  })
+  t.same(fruitChanges[1], {
+    apples: 4,
+    oranges: 6,
+    _diff: {apples: 4}
+  })
+  t.equal(changes[1].fruits, changes[0].fruits,
+    'unchanged properties are the same value')
+
+  t.end()
+})
+
+test('emits nested change', function (t) {
+  var obj = ObservStruct({
+    fruits: ObservVarhash({
+      apples: 3,
+      oranges: 5
+    }, function (val, key) {
+      return Observ(val)
+    }),
+    customers: Observ(5)
+  })
+
+  var initialState = obj()
+  var changes = []
+  var fruitChanges = []
+
+  obj(function (state) {
+    changes.push(state)
+  })
+
+  obj.fruits(function (state) {
+    fruitChanges.push(state)
+  })
+
+  obj.fruits.oranges.set(6)
+  obj.customers.set(10)
+  obj.fruits.apples.set(4)
+
+  t.equal(changes.length, 3)
+  t.equal(fruitChanges.length, 2)
+  t.notEqual(changes[0], initialState)
+  t.notEqual(changes[1], changes[0])
+  t.notEqual(changes[2], changes[1])
+  t.notEqual(fruitChanges[0], initialState.fruits)
+  t.notEqual(fruitChanges[1], fruitChanges[0])
+
+  t.same(initialState, {
+    customers: 5,
+    fruits: {
+      apples: 3,
+      oranges: 5
+    }
+  })
+  t.same(changes[0], {
+    customers: 5,
+    fruits: {
+      apples: 3,
+      oranges: 6,
+      _diff: {oranges: 6}
+    },
+    _diff: {fruits: {oranges: 6}}
+  })
+  t.same(changes[1], {
+    customers: 10,
+    _diff: {customers: 10},
+    fruits: {
+      apples: 3,
+      oranges: 6,
+      _diff: {oranges: 6}
+    }
+  })
+  t.same(changes[2], {
+    customers: 10,
+    _diff: {fruits: {apples: 4}},
+    fruits: {
+      apples: 4,
+      oranges: 6,
+      _diff: {apples: 4}
+    }
+  })
+  t.same(initialState.fruits, {
+    apples: 3,
+    oranges: 5
+  })
+  t.same(fruitChanges[0], {
+    apples: 3,
+    oranges: 6,
+    _diff: {oranges: 6}
+  })
+  t.same(fruitChanges[1], {
+    apples: 4,
+    oranges: 6,
+    _diff: {apples: 4}
+  })
+  t.equal(changes[1].fruits, changes[0].fruits,
+    'unchanged properties are the same value')
+
+  t.end()
+})
+
+test('throws for keys that hide api', function (t) {
+  var obj = ObservVarhash({foo: 'foo', bar: 'bar'})
+
+  t.throws(function () {
+    obj.put('put', {baz: 'baz'})
+  })
+  t.throws(function () {
+    obj.put('get', {baz: 'baz'})
+  })
+  t.throws(function () {
+    obj.put('delete', {baz: 'baz'})
+  })
+  t.throws(function () {
+    obj.put('_removeListeners', {baz: 'baz'})
+  })
+  t.doesNotThrow(function () {
+    obj.put('baz', {baz: 'baz'})
+  })
+
+  t.end()
+})
+
+test('Tombstone is Tombstone', function (t) {
+  var obj = ObservVarhash({
+    foo: 'foo',
+    bar: 'bar'
+  })
+  var i = 0
+  obj(function (change) {
+    if (i <= 1) return i++
+
+    t.equal(change._diff.foo, ObservVarhash.Tombstone)
+    t.end()
+  })
+
+  obj.delete('foo')
+  obj.put('foo', 'hi')
+  obj.delete('foo')
+})
